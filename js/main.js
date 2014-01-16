9a823faa43674c84be8cc35238dced49aba21152
Simon Chauvin <chauvin.simon@gmail.com>
2014-01-16 21:55:51 +0100

    First commit.

---
+++ js/main.js
@@ -0,0 +1,100 @@
+//FM.js engine
+$("#fmjsengine").hover(function () {
+    $("#fmjsengine").fadeTo(200, 0.5, function () {
+        // Animation complete.
+    });
+}, function () {
+    $("#fmjsengine").fadeTo(200, 1, function () {
+        // Animation complete.
+    });
+});
+//A Thing
+$("#athing").hover(function () {
+    $("#athing").fadeTo(200, 0.5, function () {
+        // Animation complete.
+    });
+}, function () {
+    $("#athing").fadeTo(200, 1, function () {
+        // Animation complete.
+    });
+});
+//Work Me
+$("#workme").hover(function () {
+    $("#workme").fadeTo(200, 0.5, function () {
+        // Animation complete.
+    });
+}, function () {
+    $("#workme").fadeTo(200, 1, function () {
+        // Animation complete.
+    });
+});
+//Down the Line
+$("#downtheline").hover(function () {
+    $("#downtheline").fadeTo(200, 0.5, function () {
+        // Animation complete.
+    });
+}, function () {
+    $("#downtheline").fadeTo(200, 1, function () {
+        // Animation complete.
+    });
+});
+//Overflowed
+$("#overflowed").hover(function () {
+    $("#overflowed").fadeTo(200, 0.5, function () {
+        // Animation complete.
+    });
+}, function () {
+    $("#overflowed").fadeTo(200, 1, function () {
+        // Animation complete.
+    });
+});
+//The Core of Life
+$("#thecoreoflife").hover(function () {
+    $("#thecoreoflife").fadeTo(200, 0.5, function () {
+        // Animation complete.
+    });
+}, function () {
+    $("#thecoreoflife").fadeTo(200, 1, function () {
+        // Animation complete.
+    });
+});
+//NewsLeaks
+$("#newsleaks").hover(function () {
+    $("#newsleaks").fadeTo(200, 0.5, function () {
+        // Animation complete.
+    });
+}, function () {
+    $("#newsleaks").fadeTo(200, 1, function () {
+        // Animation complete.
+    });
+});
+//Perspective
+$("#perspective").hover(function () {
+    $("#perspective").fadeTo(200, 0.5, function () {
+        // Animation complete.
+    });
+}, function () {
+    $("#perspective").fadeTo(200, 1, function () {
+        // Animation complete.
+    });
+});
+//Stream
+$("#stream").hover(function () {
+    $("#stream").fadeTo(200, 0.5, function () {
+        // Animation complete.
+    });
+}, function () {
+    $("#stream").fadeTo(200, 1, function () {
+        // Animation complete.
+    });
+});
+//Wave Me
+$("#waveme").hover(function () {
+    $("#waveme").fadeTo(200, 0.5, function () {
+        // Animation complete.
+    });
+}, function () {
+    $("#waveme").fadeTo(200, 1, function () {
+        // Animation complete.
+    });
+});
