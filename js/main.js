f2b595fd3ccb524ba8c5a4db50d0c9cebf698c75
Simon Chauvin <contact@simonchauvin.com>
2019-05-11 20:17:15 +0200

    Major rewrite. Projects have "pages" now. Added video instead of repeating the same image. Removed section titles and local links. Reorganised thumbnail grids to show in progress, selected and other projects.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,94 @@
-$(".project").find(".thumbnail" ).hover(function() {
-    $(this).parent().find(".project-title").toggle();
-    $(this).parent().find(".project-subtitle").toggle();
+var currentSection = null;
+var currentProject = null;
+var currentVideoUrl = null;
+
+var url = window.location.href;
+var startUrlPos = url.search('#project-');
+if (startUrlPos > 0)
+{
+    showProject(url.substr(startUrlPos, url.length - startUrlPos));
+}
+
+$('.project-thumbnail-img').find('a').click(
+    function(){
+        //location.href = "#home";
+    }
+);
+
+$(".project-thumbnail").hover(
+    function(){
+        $(this).find(".project-thumbnail-title").css("visibility", "visible");
+        
+        $(this).find(".project-thumbnail-img img").css({opacity: 0.2});
+        $(this).find(".project-thumbnail-img img").css({filter: 'alpha(opacity=20)'});
+    },
+    function(){
+        $(this).find(".project-thumbnail-title").css("visibility", "hidden");
+        
+        $(this).find(".project-thumbnail-img img").css({opacity: 1});
+        $(this).find(".project-thumbnail-img img").css({filter: 'alpha(opacity=100)'});
+    }
+);
+
+$('#projects').find('.project-thumbnail').click(
+    function(){ 
+        showProject($(this).find("a").attr("href"));
+        
+        return false; 
+    }
+);
+
+function showProject(projectId) {
+    currentSection = $('#projects');
+    
+    currentSection.find('.project-thumbnail').hide();
+    
+    if ($(projectId).parent().hasClass('project-in-progress'))
+    {
+        currentSection.find('#sub-section-recent').hide();
+        currentSection.find('#sub-section-earlier').hide();
+    }
+    else if ($(projectId).parent().hasClass('project-recent'))
+    {
+        currentSection.find('#sub-section-in-progress').hide();
+        currentSection.find('#sub-section-earlier').hide();
+    }
+    else if ($(projectId).parent().hasClass('project-earlier'))
+    {
+        currentSection.find('#sub-section-in-progress').hide();
+        currentSection.find('#sub-section-recent').hide();
+    }
+    
+    if ($(projectId).find('.project-video'))
+    {
+        currentVideoUrl = $(projectId).find('.project-video').attr('src');
+    }
+    
+    currentProject = projectId;
+    
+    location.href = currentProject;
+    
+    $(currentProject).show();
+}
+
+$('.back-to-projects').click(function(){ 
+    if (currentVideoUrl)
+    {
+        $(currentProject).find('.project-video').attr('src', '');
+        $(currentProject).find('.project-video').attr('src', currentVideoUrl);
+        
+        currentVideoUrl = null;
+    }
+    
+    $(currentProject).hide();
+    
+    $(currentSection).find('.project-thumbnail').show();
+    currentSection.find('.sub-section').show();
+    
+    location.href = "#";
+    
+    currentSection = null;
+    currentProject = null;
+    
+    return false;
 });
